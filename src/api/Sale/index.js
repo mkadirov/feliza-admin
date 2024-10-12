0adebcfce0a9e01913e3f6e55f6d301cbd1a9025
Kadirov1993 <mkodirjonov@mail.ru>
2024-10-12 15:45:30 +0200

    saleGroupfixed

--- src/api/Sale/index.js
+++ src/api/Sale/index.js
@@ -1,57 +1,61 @@
-import axios from 'axios'
+import axios from "axios";
 
-const apiUrl = 'https://felizabackend.uz/api/saleGroup/'
+const apiUrl = "https://felizabackend.uz/api/saleGroup/";
 
-const addSaleProduct = async(saleProduct) => {
-    try {
-      const token = localStorage.getItem('userToken');
-      console.log(token);
-      const res = await axios.post(apiUrl + 'addSaleGroup', saleProduct, {
-           headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-          },
-      });
-      if(res.status == 200) {
-        return {success: true, data: res.data}
-      }else {
-        return {success: false, message: res.message}
-      }
-    } catch (error) {
-       return {success: false, message: error.message}
-    }
-  }
-
-const getAllSaleGroups = async() => {
-    try {
-        const res = await axios.get(apiUrl + 'getAllSaleGroups')
-        if(res.status == 200) {
-          return {success: true, data: res.data}
-        } else {
-          return {success: false}
-        }
-    } catch (error) {
-        return {success: false}
-    }
-}
-
-const deleteSaleGroupByID = async(id) => {
+const addSaleProduct = async (saleProduct) => {
   try {
-    const token = localStorage.getItem('userToken');
-    const res = await axios.delete(apiUrl + 'removeSaleGroup/' + id, {
-         headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
+    const token = localStorage.getItem("userToken");
+    console.log(token);
+    const res = await axios.post(apiUrl + "addSaleGroup", saleProduct, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     });
-      if(res.status == 200) {
-        return {success: true, data: res.data}
-      } else {
-        return {success: false}
-      }
+    if (res.status == 200) {
+      return { success: true, data: res.data };
+    } else {
+      return { success: false, message: res.message };
+    }
   } catch (error) {
-      return {success: false}
+    return { success: false, message: error.message };
   }
-}
+};
 
-export {addSaleProduct, getAllSaleGroups, deleteSaleGroupByID}
+const getAllSaleGroups = async () => {
+  try {
+    const res = await axios.get(apiUrl + "getAllSaleGroups");
+    if (res.status == 200) {
+      return { success: true, data: res.data };
+    } else {
+      return { success: false };
+    }
+  } catch (error) {
+    return { success: false };
+  }
+};
+
+const deleteSaleGroupByID = async (id) => {
+  const saleGroup = {
+    saleGroupId: id,
+    saleCategoryId: 7,
+  };
+  try {
+    const token = localStorage.getItem("userToken");
+    const res = await axios.post(apiUrl + "removeSaleGroup", saleGroup, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (res.status == 200) {
+      return { success: true, data: res.data };
+    } else {
+      return { success: false };
+    }
+  } catch (error) {
+    return { success: false };
+  }
+};
+
+export { addSaleProduct, getAllSaleGroups, deleteSaleGroupByID };
